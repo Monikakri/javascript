9fc28fd041671fef228e251ebbf0c0d27ed7215c
Monikakri <monikakri394@gmail.com>
2024-01-17 09:19:35 +0530

    hello javaScript

---
+++ main.js
@@ -0,0 +1,8 @@
+var arr  = [10,"hello", false, null, undefined, 78n, Symbol(100), ()=> console.log("iam a test"),["i am tatti girl",97] ]
+console.log(arr);
+
+for(let i=0; i<arr.length; i++)
+{
+    console.log(arr[i]);
+}
+
